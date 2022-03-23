60e85cbb7341e0f0b313106886cf46eb160e71ce
kdk97 <kevinkdk97@gmail.com>
2022-03-23 09:34:45 +0100

    Sign up

--- src/App.js
+++ src/App.js
@@ -9,56 +9,73 @@ import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import ProfilePage from "./pages/ProfilePage";
+import FrontPage from "./pages/FrontPage";
 
 function App() {
-    const [showLoader, setShowLoader] = useState(true); // default value of the loader is true (loader displayed)
-    const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth")); // default value comes from localStorage
+  const [showLoader, setShowLoader] = useState(true); // default value of the loader is true (loader displayed)
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth")); // default value comes from localStorage
 
-    const auth = getAuth();
+  const auth = getAuth();
 
-    onAuthStateChanged(auth, user => {
-        if (user) {
-            //user is authenticated / signed in
-            setIsAuth(true); // set isAuth to true
-            localStorage.setItem("isAuth", true); // also, save isAuth in localStorage
-        } else {
-            // user is not authenticated / not signed in
-            setIsAuth(false); // set isAuth to false
-            localStorage.removeItem("isAuth"); // remove isAuth from localStorage
-        }
-    });
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      //user is authenticated / signed in
+      setIsAuth(true); // set isAuth to true
+      localStorage.setItem("isAuth", true); // also, save isAuth in localStorage
+    } else {
+      // user is not authenticated / not signed in
+      setIsAuth(false); // set isAuth to false
+      localStorage.removeItem("isAuth"); // remove isAuth from localStorage
+    }
+  });
 
-    // variable holding all private routes including the nav bar
-    const privateRoutes = (
-        <>
-            <Nav />
-            <Routes>
-                <Route path="/" element={<PostsPage showLoader={setShowLoader} />} />
-                <Route path="/create" element={<CreatePage showLoader={setShowLoader} />} />
-                <Route path="/posts/:id" element={<UpdatePage showLoader={setShowLoader} />} />
-                <Route path="/profile" element={<ProfilePage showLoader={setShowLoader} />} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </>
-    );
+  // variable holding all private routes including the nav bar
+  const privateRoutes = (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<PostsPage showLoader={setShowLoader} />} />
+        <Route
+          path="/create"
+          element={<CreatePage showLoader={setShowLoader} />}
+        />
+        <Route
+          path="/posts/:id"
+          element={<UpdatePage showLoader={setShowLoader} />}
+        />
+        <Route
+          path="/profile"
+          element={<ProfilePage showLoader={setShowLoader} />}
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 
-    // variable holding all public routes without nav bar
-    const publicRoutes = (
-        <Routes>
-            <Route path="/sign-in" element={<SignInPage showLoader={setShowLoader} />} />
-            <Route path="/sign-up" element={<SignUpPage showLoader={setShowLoader} />} />
-            <Route path="*" element={<Navigate to="/sign-in" />} />
-        </Routes>
-    );
+  // variable holding all public routes without nav bar
+  const publicRoutes = (
+    <Routes>
+      <Route path="/front" element={<FrontPage showLoader={setShowLoader} />} />
+      <Route
+        path="/sign-in"
+        element={<SignInPage showLoader={setShowLoader} />}
+      />
+      <Route
+        path="/sign-up"
+        element={<SignUpPage showLoader={setShowLoader} />}
+      />
+      <Route path="*" element={<Navigate to="/sign-in" />} />
+    </Routes>
+  );
 
-    // if user is authenticated, show privateRoutes, else show publicRoutes
-    // also, display or display not the <Loader/> based on showLoader state
-    return (
-        <main>
-            {isAuth ? privateRoutes : publicRoutes}
-            {showLoader && <Loader />}
-        </main>
-    );
+  // if user is authenticated, show privateRoutes, else show publicRoutes
+  // also, display or display not the <Loader/> based on showLoader state
+  return (
+    <main>
+      {isAuth ? privateRoutes : publicRoutes}
+      {showLoader && <Loader />}
+    </main>
+  );
 }
 
 export default App;
