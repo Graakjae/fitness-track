207dc13e5a46cfcabb7101b46551beb6af0a6182
Frederik Graakjær <eaafgra@students.eaaa.dk>
2022-03-24 09:37:52 +0100

    SearchBar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,21 @@
+
+
+
+export default function SearchBar({ setValue }) {
+    
+
+    /**
+     * handleClick is called when user clicks on the Article (PostCard)
+     */
+     
+        function handleSearch(event) {
+            setValue(event.target.value.toLowerCase());
+        }
+
+    return (
+        <article>
+            <input type="search" placeholder="Search" onChange={handleSearch} />;
+        </article>
+    );
+}
+
--- src/pages/OvelserPage.js
+++ src/pages/OvelserPage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import SearchBar from "../components/SearchBar";
 
 
 export default function OvelserPage() {
@@ -6,19 +7,25 @@ export default function OvelserPage() {
     
     
     const [ovelser, setOvelser] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
+    const filteredOvelser = ovelser.filter(ovelser => ovelser.name.toLowerCase().includes(searchValue));
 
-    useEffect(async () => {
+    useEffect(() => {
+        async function getOvelser() {
         const url = "https://api.jsonbin.io/b/623af66406182767437ddcdf";
         const response = await fetch(url);
         const data = await response.json();
         setOvelser(data);
+        }
+        getOvelser();
     }, []);
 
     return (
         <section className="page">
             <h1>Øvelser</h1>
+            <SearchBar setValue={setSearchValue} />
             <section>
-                {ovelser.map(ovelser => (
+                {filteredOvelser.map(ovelser => (
                     <article className="ovelser">
                         <h3>{ovelser.name}</h3>
                     </article>
