8934457db6d1bb0446113a27d118ed1b48292355
Emil5466 <emil-kramer@hotmail.dk>
2022-03-30 10:52:59 +0200

    nav-fixed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,14 +3,13 @@ import navdumbell from "../assets/img/navdumbell.png";
 import navprogression from "../assets/img/navprogression.png";
 import navworkoutplan from "../assets/img/navworkoutplan.png";
 import navprofil from "../assets/img/navprofil.png";
-import navexercises from "../assets/img/navexercises.png";
+
 
 export default function Nav() {
     return (
         <nav>
-            <NavLink to="/create"><img src= {navdumbell} alt="nav icon" ></img><p>Min træning</p></NavLink>
-            <NavLink to="/ovelser"><img src= {navexercises} alt="nav icon" ></img><p>Øvelser</p></NavLink>
             <NavLink to="/posts"><img src= {navworkoutplan} alt="nav icon" ></img><p>Træningsplaner</p></NavLink>
+            <NavLink to="/ovelser"><img src= {navdumbell} alt="nav icon" ></img><p>Øvelser</p></NavLink>    
             <NavLink to="/fremskridt"><img src= {navprogression} alt="nav icon" ></img><p>Fremskridt</p></NavLink>
             <NavLink to="/profile"><img src= {navprofil} alt="nav icon" ></img><p>Profil</p></NavLink>
         </nav>
