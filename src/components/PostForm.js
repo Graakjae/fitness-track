4551f6e1c4acc88319f3c2e6283e96488c61b1ad
Frederik Graakjær <eaafgra@students.eaaa.dk>
2022-03-30 11:28:57 +0200

    change

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,60 +1,37 @@
 import { useEffect, useState } from "react";
 
-export default function PostForm({ savePost, post }) {
+export default function PostForm({ post }) {
 
-  const [name, setname] = useState("");
-  const [body, setBody] = useState("");
+  const [name, setName] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [rating, setRating] = useState([]);
+  const [reads, setReads] = useState([]);
+
+
 
   useEffect(() => {
     if (post) {
       // if post, set the states with values from the post object.
       // The post object is a prop, passed from UpdatePage
-      setname(post.name);
+      setName(post.name);
+      setPosts(post.posts);
+      setRating(post.rating);
+      setReads(post.reads);
+
     }
   }, [post]); // useEffect is called every time post changes.
 
-  /**
-   * handleImageChange is called every time the user chooses an image in the fire system.
-   * The event is fired by the input file field in the form
-   */
-  
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formData = {
-      // create a new objebt to hold the value from states / input fields
-      name: name,
-    };
-
-    const validForm = formData.name && formData.body && formData.image; // will return false if one of the properties doesn't have a value
-    if (validForm) {
-      // if all fields/ properties are filled, then call savePost
-      savePost(formData);
-    }
-  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <h1
-          type="text"
-          value={name}
-          placeholder="Navngiv din træningsplan"
-        > </h1>
-      </label>
+    <section>
+      <h1> {post.name} </h1>
+      <p> {post.reads} </p>
+      <p> {post.rating} </p>
       
-      <label>
-        <input
-          type="text"
-          value={body}
-          placeholder="Beskriv din træningsplan"
-          onChange={(e) => setBody(e.target.value)}
-        />
-      </label>
+      
       
       <p className="text-error">{errorMessage}</p>
-      <button type="submit">Opret</button>
-    </form>
+    </section>
   );
 }
